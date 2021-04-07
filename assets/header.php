76c272b5c4a6095ef74119184933f187c7240f1f
Kozzmin <sfantu_demonic@yahoo.com>
2021-04-07 21:26:40 +0300

    07.04.2021

--- assets/header.php
+++ assets/header.php
@@ -10,9 +10,9 @@
       <title>Universul din poiana</title>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="icon" href="./resurse/photos/stuff/favicon.ico" type="image/x-icon">
+      <link rel="icon" href="resurse/photos/stuff/favicon.ico" type="image/x-icon">
 
-      <link rel="stylesheet" type="text/css" href="./resurse/css/originalGlade.css">
+      <link rel="stylesheet" type="text/css" href="resurse/css/originalGlade.css">
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
       <link
         rel="stylesheet"
