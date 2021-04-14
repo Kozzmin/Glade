1f93fff07962939b1d180d63fe658a1f43105ea0
Kozzmin <sfantu_demonic@yahoo.com>
2021-04-14 21:12:12 +0300

    14.04.2021

--- assets/account/login.php
+++ assets/account/login.php
@@ -1,10 +1,10 @@
 <div id="loginModalForm" class="modal">
 
-    <form class="modal-content animate" action="#" method="POST"> <!--includes/login.inc.php-->
-      <div class="imgcontainer">
-        <img src="resurse/photos/stuff/avatarForm.png" alt="Avatar" class="avatar">
-      </div>
-
+  <form class="modal-content animate" action="#" method="POST"> <!--includes/login.inc.php-->
+    <div class="imgcontainer">
+      <img src="resurse/photos/stuff/avatarForm.png" alt="Avatar" class="avatar">
+    </div>
+    <section class="form-container">
       <div class="container">                     
         <input type="text" placeholder="Introduceti nume de utilizator" name="uid" autocomplete required>
         <input type="password" placeholder="Introduceti parola" name="pwd" required> 
@@ -15,6 +15,7 @@
         <span class="pwd">Ai uitat <a href="#">parola?</a></span>
         <!--de facut ai uitat parola pagina interogare sql/baza de date 03/03/2021 -->                
       </div>
-    </form>
+    </section>
+  </form>
 
   </div>
